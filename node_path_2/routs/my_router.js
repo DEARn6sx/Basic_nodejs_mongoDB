2e8d97405bcdda90352536b872ed5771a3438730
DEARn6sx <nantawats.study@gmail.com>
2023-12-12 01:23:58 +0700

    static file

--- node_path_2/routs/my_router.js
+++ node_path_2/routs/my_router.js
@@ -2,28 +2,4 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-
-const pathPage = path.join(`${__dirname}/../templates`)
-
-router.get('/',(req,res)=>{
-    res.status(200)
-    res.type(`text/html`) //รูปแบบการ response
-    res.sendFile(`${pathPage}/index.html`)
-})
-router.get('/product/:id',(req,res)=>{
-    const myParam_id = req.params.id
-    if (myParam_id ==='1') {
-        res.sendFile(`${pathPage}/product1.html`)
-    }
-    else if (myParam_id ==='2') {
-        res.sendFile(`${pathPage}/product2.html`)
-    }
-    else if (myParam_id ==='3') {
-        res.sendFile(`${pathPage}/product3.html`)
-    }
-    else {
-        res.redirect('/')
-    }
-})
-
 module.exports = router
--- node_path_2/start_server1.js
+++ node_path_2/start_server1.js
@@ -1,10 +1,7 @@
 const express = require('express')
-//ใช้ router จัดการ routing ที่มีความซับซ้อน
-const router = require('./routs/my_router')
-const app = express() 
-
-app.use(router)
-
-app.listen(4000,()=>{
-    console.log(`start server prot: 4000`);
+const path = require('path')
+const app = express()
+app.use(express.static(path.join(`${__dirname}/public`)))
+app.listen(5000,()=>{
+    console.log(`start server prot: 5000`);
 })
