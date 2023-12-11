b957dc230de42f3dbeb7cce1d56acebce9881a5b
DEARn6sx <nantawats.study@gmail.com>
2023-12-11 13:37:25 +0700

    basic html template

--- node_path_1/start_server1.js
+++ node_path_1/start_server1.js
@@ -1,12 +1,15 @@
 const http = require('http')
+const fs = require('fs')
+const indexpage = fs.readFileSync(`${__dirname}/templates/index.html`)
+const productpage = fs.readFileSync(`${__dirname}/templates/product1.html`,'utf-8')
 
 const server = http.createServer(function(req, res) {
     const pathname = req.url
     console.log(`url = ${pathname}`);
     if (pathname === '/' || pathname === 'home') {
-        res.end('Heeloo homepage')
+        res.end(indexpage)
     }else if (pathname === '/product') {
-        res.end('hello product')
+        res.end(productpage)
     }else{ 
         res.writeHead(404)
         res.end('not found!!')
