48b537cb87d495783c8320f1db3b8a2f229e4fe0
DEARn6sx <nantawats.study@gmail.com>
2023-12-12 01:33:22 +0700

    add template kshop

--- node_path_2/start_server1.js
+++ node_path_2/start_server1.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(`${__dirname}/public`)))
+app.use(express.static(path.join(`${__dirname}/kshop_templates`)))
 app.listen(5000,()=>{
     console.log(`start server prot: 5000`);
 })
