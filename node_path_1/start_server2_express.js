9e2ec2c8dee5844ab427832eec894cfc8b5edc64
DEARn6sx <nantawats.study@gmail.com>
2023-12-11 19:42:46 +0700

    basic express & roting

--- node_path_1/start_server2_express.js
+++ node_path_1/start_server2_express.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express() //เรียกใช้งาน express
 
 //การใช้งานตัวระบบ routing ในตอนเริ่มต้น * ต้องเขียนก่อน app.listen
-app.use((req,res)=>{
-    res.send(`hello express.js`)
+//app.get method กำหนดการทำงานของ req ที่ถูกส่งเข้ามา เข่นการส่งค่ามาพร้อมกับ url
+//callback func ใน method get ใช้กำหนดการตอบสนองที่ส่ง request เข้ามา
+app.get('/',(req,res)=>{
+    res.send(`hello express22222.js`)
+})
+app.get('/product',(req,res)=>{
+    res.send(`hello product.js`)
 })
 
 app.listen(8080,()=>{
