d4f628d6e02e36899848648fcf3d41fb29f0e8aa
DEARn6sx <nantawats.study@gmail.com>
2023-12-11 19:52:28 +0700

    basic express : module path

--- node_path_1/start_server2_express.js
+++ node_path_1/start_server2_express.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express() //เรียกใช้งาน express
+
+//อ้างอิงตำแหน่งไฟล์
+const pathPage = path.join(`${__dirname}/templates`)
 
 //การใช้งานตัวระบบ routing ในตอนเริ่มต้น * ต้องเขียนก่อน app.listen
 //app.get method กำหนดการทำงานของ req ที่ถูกส่งเข้ามา เข่นการส่งค่ามาพร้อมกับ url
 //callback func ใน method get ใช้กำหนดการตอบสนองที่ส่ง request เข้ามา
 app.get('/',(req,res)=>{
-    res.send(`hello express22222.js`)
+    res.status(200)
+    res.type(`text/html`) //รูปแบบการ response
+    res.sendFile(`${pathPage}/index.html`)
 })
 app.get('/product',(req,res)=>{
-    res.send(`hello product.js`)
+    res.status(200)
+    res.type(`text/html`) //รูปแบบการ response
+    res.sendFile(`${pathPage}/product1.html`)
 })
 
 app.listen(8080,()=>{
