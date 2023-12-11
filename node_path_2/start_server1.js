54a0eb702fcf961b5dc29e7740f976305e70e7ec
DEARn6sx <nantawats.study@gmail.com>
2023-12-12 03:27:09 +0700

    Basic Object  EJS

--- node_path_2/start_server1.js
+++ node_path_2/start_server1.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const router = require('./routs/my_router2_array')
+const router = require('./routs/my_router3_object')
 const app = express()
 
 //ตั้งค่า engine สำหรับรันแอป
@@ -8,6 +8,7 @@ app.set('views', path.join(`${__dirname}/views`))
 app.set('view engine','ejs')
 
 app.use(router)
+app.use(express.static(path.join(`${__dirname}/ks_templates`)))
 app.listen(5000,()=>{
     console.log(`start server port: 5000`);
 })
