d31940b55767ff50d20bd03dd54c18061cca31ec
DEARn6sx <nantawats.study@gmail.com>
2023-12-13 01:04:41 +0700

    POST Data

---
+++ node_path_2/start_server2.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const router = require('./routs/my_router7_post')
+const app = express()
+
+//ตั้งค่า engine สำหรับรันแอป
+app.set('views', path.join(`${__dirname}/views`))
+app.set('view engine','ejs') 
+app.use(express.urlencoded({extended:false})) //ส่งข้อมูลจาก form แบบ post มา encode เพื่อเอาข้อมูลมาใช้
+
+app.use(router)
+app.use(express.static(path.join(`${__dirname}/public`))) //ใช้งานfolder ks_templates 
+app.listen(8000,()=>{
+    console.log(`start server port: 8000`);
+})
